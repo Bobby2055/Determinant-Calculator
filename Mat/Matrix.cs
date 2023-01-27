432857b48f57bae1e94fcce8de2047a836c3adf0
Bobby-Laptop <Bobby-Laptop@BOBBY-LAPTOP>
2023-01-27 18:55:25 +0200

    Add project files.

---
+++ Mat/Matrix.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mat
+{
+    public class Matrix
+    {
+        public static void RandomMatrix(int[,] A)
+        {
+            Random rnd = new Random();
+            for(int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A.GetLength(0); j++)
+                {
+                    A[i, j] = rnd.Next(1, 999);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        
+        public static void PrintMatrix(int[,] A)
+        {
+            int[,] Rezult = new int[A.GetLength(0), A.GetLength(1)];
+            for (int i = 0; i < A.GetLength(0); i++)
+            {
+                for (int j = 0; j < A.GetLength(1); j++)
+                {
+                    Console.Write("{0,5}", A[i,j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        public static int[,] SumMatrix(int[,] A, int[,] B)
+        {
+            if (A.GetLength(0) == B.GetLength(0) || (A.GetLength(1) == B.GetLength(1)))
+            {
+                int[,] Rezult = new int[A.GetLength(0), A.GetLength(1)];
+                for (int i = 0; i < A.GetLength(0); i++)
+                {
+                    for (int j = 0; j < A.GetLength(0); j++)
+                    {
+                        Rezult[i, j] = A[i, j] + B[i, j];
+                    }
+                }
+                return Rezult;
+            }
+            else
+            {
+                throw new Exception("Ne moje da se izvarsi deistvieto ");
+            }
+        }
+    }
+}
---
+++ Matematika/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mat;
+
+namespace Matematika
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            int[,] A = new int[2, 3];
+            Matrix.RandomMatrix(A);
+            Matrix.PrintMatrix(A);
+
+            int[,] B = new int[2, 3];
+            Matrix.RandomMatrix(A);
+            Matrix.PrintMatrix(A);
+            
+            Matrix.PrintMatrix(Matrix.SumMatrix(A,B));
+
+        }
+    }
+}
